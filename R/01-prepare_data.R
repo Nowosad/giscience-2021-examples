6eaa578ee137b0afeafb70e592768ab12912035e
Jakub Nowosad <tupiszakaczki@gmail.com>
2021-06-20 12:01:31 +0200

    cleans code

--- R/01-prepare_data.R
+++ R/01-prepare_data.R
@@ -6,10 +6,7 @@ library(FedData)
 
 AOI = aoi_get(state = "OH")
 AOI_proj = st_transform(AOI, "EPSG:32617")
-nlcd = get_nlcd(
-  template = as(AOI_proj, "Spatial"),
-  label = "ohio"
-)
+nlcd = get_nlcd(template = as(AOI_proj, "Spatial"), label = "ohio")
 nlcd = rast(nlcd)
 AOI_proj2 = st_transform(AOI_proj, crs(nlcd))
 nlcd_oh = crop(nlcd, vect(AOI_proj2))
